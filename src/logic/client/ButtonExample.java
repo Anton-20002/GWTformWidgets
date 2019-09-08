b152842c0cb76a9d784cb7da76d9b9107ed866af
Anton <austere@list.ru>
2019-09-08 12:51:40 +0300

    next v.1.0.1

---
+++ src/logic/client/ButtonExample.java
@@ -0,0 +1,9 @@
+package logic.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+
+public class ButtonExample implements EntryPoint {
+  public void onModuleLoad() {
+  }
+}
